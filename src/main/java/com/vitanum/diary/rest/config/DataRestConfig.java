ee9b2255defef6bd7b98d70a0acf8a06528e4adb
Cristina Dinescu <crina91@gmail.com>
2020-03-26 19:58:11 +0200

    Expose the id of DiaryEntry; it will be visible in the JSON/XML response of the diary service.

---
+++ src/main/java/com/vitanum/diary/rest/config/DataRestConfig.java
@@ -0,0 +1,41 @@
+package com.vitanum.diary.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        entities.stream().forEach(entityType -> {
+            System.out.println(entityType.getJavaType());
+            entityClasses.add(entityType.getJavaType());
+        });
+
+        config.exposeIdsFor(entityClasses.toArray(new Class[0]));
+    }
+}
