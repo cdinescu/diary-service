71818704c4ab15a53645b0f8c9bd1b96e171c74a
Cristina Dinescu <crina91@gmail.com>
2020-06-14 12:44:44 +0300

    Temporary: - @CrossOrigin on REST controller
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

--- src/main/java/com/vitanum/diary/repository/DiaryEntryRepository.java
+++ src/main/java/com/vitanum/diary/repository/DiaryEntryRepository.java
@@ -3,11 +3,13 @@ package com.vitanum.diary.repository;
 import com.vitanum.diary.entitities.DiaryEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin
 public interface DiaryEntryRepository extends JpaRepository<DiaryEntry, Integer> {
 
     List<DiaryEntry> findByUsernameAndDate(@RequestParam("username") String username,
